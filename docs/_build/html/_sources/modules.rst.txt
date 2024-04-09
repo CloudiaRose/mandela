6bc3af93e2c211428f3aa9184adf4b697d027407
Cloudia <cloudia.barr@live.com>
2024-04-09 05:39:18 +0200

    added sphinx documentation files

---
+++ docs/_build/html/_sources/modules.rst.txt
@@ -0,0 +1,13 @@
+mandela
+=======
+
+.. toctree::
+   :maxdepth: 4
+
+   admin
+   apps
+   forms
+   models
+   tests
+   urls
+   views
--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,9 @@ release = '00.00.01'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -28,10 +30,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mandela.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mandela_website.settings'
 django.setup()
